28da443306bd397029cdbd16483a9980efee9ad6
VINODKUMARREDDYM <vinodkumarreddy0402@gmail.com>
2018-11-25 08:43:28 +0530

    "ads2_m12_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2_Exam-2/Solution.java
+++ ADS-2_Exam-2/Solution.java
@@ -5,8 +5,97 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Scanner;
-import javafx.util.Pair;
 
+
+class Matrix
+{
+	int V;
+	int mat[][];
+	Matrix(int V)
+	{
+		this.V=V;
+		mat=new int [V][V];
+		for(int i=0;i<V;i++)
+			for(int j=0;j<V;j++)
+				mat[i][j]=0;
+	}
+	public void addMatrix(int i,int j,int weight)
+	{
+		mat[i][j]=weight;
+		mat[j][i]=weight;
+	}
+}
+class ShortestPath 
+{ 
+	int V;
+    int No_Parent=-1;
+    ShortestPath(int v)
+    {
+    	this.V=v;
+    }
+    int minDistance(int dist[], Boolean sptSet[]) 
+    { 
+      
+        int min = Integer.MAX_VALUE, min_index=-1;
+        
+  
+        for (int v = 0; v < V; v++) 
+            if (sptSet[v] == false && dist[v] <= min) 
+            { 
+                min = dist[v]; 
+                min_index = v; 
+            } 
+  
+        return min_index; 
+    } 
+    
+   
+    void printSolution(int dist[], int n,int dest) 
+    { 
+      //  System.out.println("Vertex   Distance from Source"); 
+       int count=0;
+        for (int i = 0; i < V; i++) {
+            //System.out.println(dist[i]); 
+        
+        	if(i==dest&&dist[i]!=Integer.MAX_VALUE)
+        	{
+        		count++;
+        		System.out.println((float)dist[i]);
+        	}
+        }
+        if(count==0)
+        {
+        	System.out.println("No Path Found.\r\n" + "");
+        }
+    } 
+   
+    
+    void dijkstra(int graph[][], int src,int dest) 
+    { 
+        int dist[] = new int[V];  
+        Boolean sptSet[] = new Boolean[V]; 
+        for (int i = 0; i < V; i++) 
+        { 
+            dist[i] = Integer.MAX_VALUE; 
+            sptSet[i] = false; 
+        }
+        dist[src] = 0; 
+        int [] Parents=new int [V];
+        Parents[src]=No_Parent;
+      
+        for (int count = 0; count < V-1; count++) 
+        { 
+        	int u = minDistance(dist, sptSet); 
+        	sptSet[u] = true; 
+            for (int v = 0; v < V; v++) 
+                if (!sptSet[v] && graph[u][v]!=0 && 
+                        dist[u] != Integer.MAX_VALUE && 
+                        dist[u]+graph[u][v] < dist[v]) 
+                    dist[v] = dist[u] + graph[u][v]; 
+        } 
+        printSolution(dist, V,dest); 
+    }
+}
 
 class Edge 
 {
